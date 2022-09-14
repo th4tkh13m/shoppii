f01fea1b643b985c9c1ca4b10d413f7e58c41df5
Khiem Ton <29520244+th4tkh13m@users.noreply.github.com>
2022-09-14 17:20:17 +0700

    Update fetch.sh

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl -fsSL https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.82/bin/apache-tomcat-8.5.82.tar.gz
+wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.82/bin/apache-tomcat-8.5.82.tar.gz
 tar -xvf apache-tomcat-8.5.82.tar.gz
 cp ./tomcat/tomcat-users.xml ./apache-tomcat-8.5.82/conf/
 ./apache-tomcat-8.5.82/bin/catalina.sh start
