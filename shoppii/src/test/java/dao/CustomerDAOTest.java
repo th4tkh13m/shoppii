be6975860192ba7ae446600c3b720e6e572011de
Khiem <th4tkh13m@protonmail.com>
2022-09-29 22:41:23 +0700

    add login test

--- shoppii/src/test/java/dao/CustomerDAOTest.java
+++ shoppii/src/test/java/dao/CustomerDAOTest.java
@@ -1,5 +1,6 @@
 package dao;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -14,6 +15,8 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+
 import org.apache.ibatis.jdbc.ScriptRunner;
 import org.junit.After;
 import org.junit.Before;
@@ -47,7 +50,6 @@ public class CustomerDAOTest extends DBInfo {
         connection = db.getConnection();
         Statement statement = connection.createStatement();
         statement.executeUpdate("CREATE DATABASE " + dbName);
-        System.out.println("DATABASE Created Succesfully.");
 
 
         db = new DBConnect(className, sqlType, host, dbName, userName, password);
@@ -62,22 +64,28 @@ public class CustomerDAOTest extends DBInfo {
     
     @Test
     public void testInsertCustomer() {
-            Customer customer = CustomerDAO.createCustomer(1, "An", "an@gmail.com", "0123456789",
-                    Date.valueOf("2002-10-10"), true, "abc123");
+        System.out.println("TEST: Customer Insert.");
+        Customer customer = CustomerDAO.createCustomer(1, "An", "an@gmail.com", "0123456789",
+                Date.valueOf("2002-10-10"), true, "abc123");
 
-            CustomerDAO.insertCustomer(customer, null, null, connection);
+        assertTrue(CustomerDAO.insertCustomer(customer, null, null, connection));
+        System.out.println("Test passed.");
+        
     }
 
     @Test
     public void testGetCustomer() {
+        System.out.println("TEST: Get Customer.");
         Customer customer = CustomerDAO.createCustomer(1, "An", "an@gmail.com", "0123456789",
                     Date.valueOf("2002-10-10"), true, "abc123");
         CustomerDAO.insertCustomer(customer, null, null, connection);
         assertEquals(customer, CustomerDAO.getCustomerFromId(1, connection));
+        System.out.println("Test passed.");
     }
 
     @Test
     public void testUpdateCustomer() {
+        System.out.println("TEST: Update Customer Information.");
         Customer customer = CustomerDAO.createCustomer(1, "An", "an@gmail.com", "0123456789",
                     Date.valueOf("2002-10-10"), true, "abc123");
         CustomerDAO.insertCustomer(customer, null, null, connection);
@@ -85,19 +93,23 @@ public class CustomerDAOTest extends DBInfo {
         customer.setMail("binh@gmail.com");
         Customer updatedCustomer = CustomerDAO.updateInfo(customer, connection);
         assertEquals(customer, updatedCustomer);
+        System.out.println("Test passed.");
     }
 
     @Test
     public void createRequestTest() {
+        System.out.println("TEST: Create new Shop Request.");
         Customer customer = CustomerDAO.createCustomer(1, "An", "an@gmail.com", "0123456789",
                     Date.valueOf("2002-10-10"), true, "abc123");
         CustomerDAO.insertCustomer(customer, null, null, connection);
         assertTrue(CustomerDAO
             .createRequest(customer, "Apple", "US", "Sell overpriced things", connection));
+        System.out.println("Test passed.");
     }
 
     @Test
     public void getRequestsTest() {
+        System.out.println("TEST: Get Shop Requests list.");
         Customer customer = CustomerDAO.createCustomer(1, "An", "an@gmail.com", "0123456789",
                     Date.valueOf("2002-10-10"), true, "abc123");
         CustomerDAO.insertCustomer(customer, null, null, connection);
@@ -113,17 +125,57 @@ public class CustomerDAOTest extends DBInfo {
 
         CustomerDAO
             .createRequest(customer, "HP", "US", "Sell normal things", connection);
-        assertNotNull(CustomerDAO.getRequests(customer, connection));
+        ArrayList<String> names = new ArrayList<>();
+        
         for (ShopRequest request : CustomerDAO.getRequests(customer, connection)) {
-            System.out.println(request.getName());
+            names.add(request.getName());
         }
+
+        String[] expected = {"Apple", "HP"};
+        assertArrayEquals(expected, names.toArray());
+        System.out.println("Test Passed.");
+    }
+
+    @Test
+    public void checkLoginByMailTest1() {
+        Customer customer = CustomerDAO.createCustomer(1, "An", "an@gmail.com", "0123456789",
+                    Date.valueOf("2002-10-10"), true, "abc123");
+        CustomerDAO.insertCustomer(customer, null, null, connection);
+
+        assertTrue(CustomerDAO.checkLogin("an@gmail.com", null, "abc123", connection));
+    }
+
+    @Test
+    public void checkLoginByMailTest2() {
+        Customer customer = CustomerDAO.createCustomer(1, "An", "an@gmail.com", "0123456789",
+                    Date.valueOf("2002-10-10"), true, "abc123");
+        CustomerDAO.insertCustomer(customer, null, null, connection);
+
+        assertTrue(!CustomerDAO.checkLogin("an@gmail.com", null, "abc1", connection));
+    }
+    
+    @Test
+    public void checkLoginByPhoneTest1() {
+        Customer customer = CustomerDAO.createCustomer(1, "An", "an@gmail.com", "0123456789",
+                    Date.valueOf("2002-10-10"), true, "abc123");
+        CustomerDAO.insertCustomer(customer, null, null, connection);
+
+        assertTrue(!CustomerDAO.checkLogin(null, "0123456789", "abc1", connection));
+    }
+
+    @Test
+    public void checkLoginByPhoneTest2() {
+        Customer customer = CustomerDAO.createCustomer(1, "An", "an@gmail.com", "0123456789",
+                    Date.valueOf("2002-10-10"), true, "abc123");
+        CustomerDAO.insertCustomer(customer, null, null, connection);
+
+        assertTrue(CustomerDAO.checkLogin(null, "0123456789", "abc123", connection));
     }
 
     @After
     public void tearDown() throws SQLException {
         Statement statement = connection.createStatement();
         statement.execute("DROP DATABASE " + dbName);
-        System.out.println("DATABASE Dropped Succesfully.");
         connection.close();
     }
 }
