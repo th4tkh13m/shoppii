4e807ffbdca7a9adb82e2968ec0eb1140a19f47d
Khiem <th4tkh13m@protonmail.com>
2022-10-03 15:43:49 +0700

    feat: test register

--- shoppii/src/main/java/controllers/authentication/RegisterServlet.java
+++ shoppii/src/main/java/controllers/authentication/RegisterServlet.java
@@ -1,6 +1,7 @@
 package controllers.authentication;
 
 import java.io.IOException;
+import java.sql.Connection;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,12 +11,15 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
 
 import dao.CustomerDAO;
+import dbconnect.DBConnect;
 import model.Customer;
 
 public class RegisterServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Gson gson = new Gson();
+        DBConnect db = new DBConnect();
+        Connection connection = db.getConnection();
         String email = null;
         String phone = null;
         if (req.getParameter("email") != null) {
@@ -25,12 +29,9 @@ public class RegisterServlet extends HttpServlet {
             phone = req.getParameter("phone");
         }
         String password = req.getParameter("password");
-        String name = req.getParameter("name");
-        String dob = req.getParameter("dob");
-        String sex = req.getParameter("sex");
         // Customer customer = CustomerDAO.createCustomer(name, email, phone, null,
         // false, password);
-        Customer customer = new Customer(name, email, phone, null, false, password);
+        Customer customer = CustomerDAO.register(email, phone, password, connection);
         // db here
         String json = gson.toJson(customer);
         resp.setContentType("application/json");
