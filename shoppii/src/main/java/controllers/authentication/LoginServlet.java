682cc128f1e3abcfe64747d39f6c21f39688ebe7
VVD <daovo739@gmail.com>
2022-10-03 22:53:39 +0700

    login controller

--- shoppii/src/main/java/controllers/authentication/LoginServlet.java
+++ shoppii/src/main/java/controllers/authentication/LoginServlet.java
@@ -1,12 +1,19 @@
 package controllers.authentication;
 
 import java.io.IOException;
+import java.sql.Connection;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import dao.CustomerDAO;
+import dbconnect.DBConnect;
+import errors.ErrorHandle;
 
 @MultipartConfig(fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
         maxFileSize = 1024 * 1024 * 1, // 1 MB
@@ -16,6 +23,33 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // TODO Auto-generated method stub
-        super.doPost(req, resp);
+        Gson gson = new Gson();
+        resp.setContentType("application/json");
+        try {
+            DBConnect db = new DBConnect();
+            Connection connection = db.getConnection();
+            String email = null;
+            String phone = null;
+            if (req.getParameter("email") != null) {
+                email = req.getParameter("email");
+            }
+            if (req.getParameter("phone") != null) {
+                phone = req.getParameter("phone");
+            }
+            String password = req.getParameter("password");
+            String rePassword = req.getParameter("rePassword");
+            if (password.equals(rePassword)) {
+                // db here
+                CustomerDAO.checkLogin(email, phone, rePassword, null);
+            } else {
+                resp.setStatus(400);
+                resp.getOutputStream().println(gson.toJson(new ErrorHandle("Password not match", 400)));
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            resp.setStatus(500);
+            resp.getOutputStream().println(gson.toJson(new ErrorHandle("Something went wrong", 500)));
+        }
+
     }
 }
