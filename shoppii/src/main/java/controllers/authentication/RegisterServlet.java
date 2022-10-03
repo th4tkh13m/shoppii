159cf4802b09760cbbc4aa43126b4b034116a16c
VVD <daovo739@gmail.com>
2022-10-03 21:18:57 +0700

    a

--- shoppii/src/main/java/controllers/authentication/RegisterServlet.java
+++ shoppii/src/main/java/controllers/authentication/RegisterServlet.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.sql.Connection;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +16,10 @@ import dao.CustomerDAO;
 import dbconnect.DBConnect;
 import model.Customer;
 
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
+        maxFileSize = 1024 * 1024 * 1, // 1 MB
+        maxRequestSize = 1024 * 1024 * 1 // 1 MB
+)
 public class RegisterServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -22,19 +28,16 @@ public class RegisterServlet extends HttpServlet {
         Connection connection = db.getConnection();
         String email = null;
         String phone = null;
-        String test = req.getParameter("test");
         if (req.getParameter("email") != null) {
             email = req.getParameter("email");
         }
         if (req.getParameter("phone") != null) {
             phone = req.getParameter("phone");
         }
-        String password = "aaa";
-        // Customer customer = CustomerDAO.createCustomer(name, email, phone, null,
-        // false, password);
+        String password = req.getParameter("password");
         Customer customer = CustomerDAO.register(email, phone, password, connection);
         // db here
-        String json = gson.toJson(test);
+        String json = gson.toJson(customer);
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
         resp.getOutputStream().println(json);
