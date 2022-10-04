60d01c43945927811382f158b2875a8bd747cb03
Hoai Nhan <hoainhan.lhp@gmail.com>
2022-10-04 18:18:31 +0700

    product

--- shoppii/src/main/java/controllers/shop/ProductsShopServlet.java
+++ shoppii/src/main/java/controllers/shop/ProductsShopServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.sql.Connection;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,6 +16,10 @@ import dbconnect.DBConnect;
 import errors.ErrorHandle;
 import model.Product;
 
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
+        maxFileSize = 1024 * 1024 * 1, // 1 MB
+        maxRequestSize = 1024 * 1024 * 1 // 1 MB
+)
 public class ProductsShopServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -23,13 +28,14 @@ public class ProductsShopServlet extends HttpServlet {
         Connection connection = db.getConnection();
         resp.setContentType("application/json");
         try {
+            int shopId = Integer.parseInt(req.getParameter("shopId"));
             String name = req.getParameter("name");
             int price = Integer.parseInt(req.getParameter("price"));
             int quantity = Integer.parseInt(req.getParameter("quantity"));
             String cat = req.getParameter("category");
             String des = req.getParameter("description");
-            // Product product = ProductDAO.addProduct(new Product(name, price, quantity, cat, des), connection);
-            String json = gson.toJson(new Product(name, price, quantity, cat, des));
+            Product product = ProductDAO.addProduct(new Product(shopId, name, price, quantity, cat, des), connection);
+            String json = gson.toJson(product);
             resp.setStatus(201);
             resp.getOutputStream().println(json);
         } catch (Exception e) {
