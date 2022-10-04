2cfbeaa6041c1c9c0cad3d944b7546e588cc8b8c
Khiem <th4tkh13m@protonmail.com>
2022-10-04 08:46:48 +0700

    feat: check login for login servlet and add to web.xml

--- shoppii/src/main/java/controllers/authentication/LoginServlet.java
+++ shoppii/src/main/java/controllers/authentication/LoginServlet.java
@@ -14,6 +14,7 @@ import com.google.gson.Gson;
 import dao.CustomerDAO;
 import dbconnect.DBConnect;
 import errors.ErrorHandle;
+import model.Customer;
 
 @MultipartConfig(fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
         maxFileSize = 1024 * 1024 * 1, // 1 MB
@@ -37,14 +38,13 @@ public class LoginServlet extends HttpServlet {
                 phone = req.getParameter("phone");
             }
             String password = req.getParameter("password");
-            String rePassword = req.getParameter("rePassword");
-            if (password.equals(rePassword)) {
-                // db here
-                CustomerDAO.checkLogin(email, phone, rePassword, connection);
-            } else {
-                resp.setStatus(400);
-                resp.getOutputStream().println(gson.toJson(new ErrorHandle("Password not match", 400)));
-            }
+            Customer customer = CustomerDAO.checkLogin(email, phone, password, connection);
+            if (customer != null) {
+                String json = gson.toJson(customer);
+                resp.setStatus(200);
+                resp.getOutputStream().println(json);
+                
+            } 
         } catch (Exception e) {
             // TODO: handle exception
             resp.setStatus(500);
