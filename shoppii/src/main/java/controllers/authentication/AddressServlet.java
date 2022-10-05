83125f1f6b0280ee6daa17c70ee90513de7b8396
Potterha <quanpotterha@gmail.com>
2022-10-05 11:15:00 +0700

    Address

--- shoppii/src/main/java/controllers/authentication/AddressServlet.java
+++ shoppii/src/main/java/controllers/authentication/AddressServlet.java
@@ -2,6 +2,7 @@ package controllers.authentication;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -31,13 +32,79 @@ public class AddressServlet extends HttpServlet {
         Connection connection = db.getConnection();
         resp.setContentType("application/json");
         try {
-    
+
             int userId = Integer.parseInt(req.getParameter("userId"));
             String receiverAddress = req.getParameter("receiverAddress");
             String receiverName = req.getParameter("receiverName");
             String receiverPhone = req.getParameter("receiverPhone");
-            
+
             Address address = AddressDAO.addAddress(userId, receiverAddress, receiverName, receiverPhone,
+                    connection);
+            String json = gson.toJson(address);
+            resp.setStatus(201);
+            resp.getOutputStream().println(json);
+        } catch (Exception e) {
+            // TODO: handle exception
+            resp.setStatus(500);
+            resp.getOutputStream().println(gson.toJson(new ErrorHandle("Something went wrong", 500)));
+        }
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Gson gson = new Gson();
+        DBConnect db = new DBConnect();
+        Connection connection = db.getConnection();
+        resp.setContentType("application/json");
+        try {
+            int userId = Integer.parseInt(req.getParameter("userId"));
+            ArrayList<Address> address = AddressDAO.getAddressOfUser(userId, connection);
+            String json = gson.toJson(address);
+            resp.setStatus(201);
+            resp.getOutputStream().println(json);
+        } catch (Exception e) {
+            // TODO: handle exception
+            resp.setStatus(500);
+            resp.getOutputStream().println(gson.toJson(new ErrorHandle("Something went wrong", 500)));
+        }
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Gson gson = new Gson();
+        DBConnect db = new DBConnect();
+        Connection connection = db.getConnection();
+        resp.setContentType("application/json");
+        try {
+            int userId = Integer.parseInt(req.getParameter("userId"));
+            int addressId = Integer.parseInt(req.getParameter("addressId"));
+            AddressDAO.deleteAddress(addressId, userId, connection);
+            doGet(req, resp);
+        } catch (Exception e) {
+            // TODO: handle exception
+            resp.setStatus(500);
+            resp.getOutputStream().println(gson.toJson(new ErrorHandle(e.toString(), 500)));
+        }
+
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Gson gson = new Gson();
+        DBConnect db = new DBConnect();
+        Connection connection = db.getConnection();
+        resp.setContentType("application/json");
+        try {
+
+            int userId = Integer.parseInt(req.getParameter("userId"));
+            int addressId = Integer.parseInt(req.getParameter("addressId"));
+            String receiverAddress = req.getParameter("receiverAddress");
+            String receiverName = req.getParameter("receiverName");
+            String receiverPhone = req.getParameter("receiverPhone");
+
+            Address address = AddressDAO.updateAddress(addressId, userId, receiverAddress, receiverName, receiverPhone,
                     connection);
             String json = gson.toJson(address);
             resp.setStatus(201);
