066820ce551b31c7b6301edff645e8a98c56ba3b
Potter <quanpotterha@gmail.com>
2022-10-11 01:45:43 +0700

    CreateOrder

--- shoppii/src/main/java/controllers/user/OrderServlet.java
+++ shoppii/src/main/java/controllers/user/OrderServlet.java
@@ -10,8 +10,10 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
 
+import dao.OrderDAO;
 import dbconnect.DBConnect;
 import errors.ErrorHandle;
+import model.Order;
 
 
 public class OrderServlet extends HttpServlet {
@@ -22,12 +24,17 @@ public class OrderServlet extends HttpServlet {
         try {
             DBConnect db = new DBConnect();
             Connection connection = db.getConnection();
+            int product_id = Integer.parseInt(req.getParameter("product_id"));
+            int quantity = Integer.parseInt(req.getParameter("quantity"));
+            int price = Integer.parseInt(req.getParameter("price"));
             int user_id = Integer.parseInt(req.getParameter("user_id"));
             String payment_method = req.getParameter("payment_method");
             String status = req.getParameter("status");
             int addressId = Integer.parseInt(req.getParameter("address_id")) ;
-            
-            
+            Order order = OrderDAO.addOrder(product_id, quantity, price, user_id, payment_method, status, addressId, connection);
+            String json = gson.toJson(order);
+            resp.setStatus(201);
+            resp.getOutputStream().println(json);
         } catch (Exception e) {
             // TODO: handle exception
             resp.setStatus(500);
--- shoppii/src/main/java/dao/OrderDAO.java
+++ shoppii/src/main/java/dao/OrderDAO.java
@@ -3,6 +3,8 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.Time;
 import java.util.ArrayList;
 import java.util.logging.Level;
@@ -13,6 +15,45 @@ import model.Product;
 import model.Shop;
 
 public class OrderDAO {
+    public static Order addOrder(int product_id, int quantity, int price, int user_id, String payment_method,
+            String status, int address_id, Connection connection) {
+        try {
+            String sql = "INSERT INTO `Cotain` (product_id, quantity, price) VALUES (?,?,?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, product_id);
+            statement.setInt(2, quantity);
+            statement.setInt(3, price);
+            statement.execute();
+            String sql1 = "INSERT INTO `Order` (order_id,user_id, payment_method,status,time, address_id) VALUES(?,?,?,?,?,?)";
+            PreparedStatement statement1 = connection.prepareStatement(sql);
+            statement1.setInt(1, selectMaxOrder(connection));
+            statement1.setInt(2, user_id);
+            statement1.setString(3, payment_method);
+            statement1.setString(4, status);
+            statement1.setTime(5, new Time(System.currentTimeMillis()));
+            statement1.setInt(6, address_id);
+            statement1.execute();
+
+            return new Order(selectMaxOrder(connection), user_id, payment_method, status,
+                    new Time(System.currentTimeMillis()), address_id);
+
+        } catch (Exception e) {
+            Logger.getLogger(CustomerDAO.class.getName()).log(Level.SEVERE, null, e);
+            return null;
+        }
+    }
+
+    private static int selectMaxOrder(Connection connection) throws SQLException {
+        String sql = "SELECT MAX(order_id) FROM `Order`";
+        PreparedStatement statement2 = connection.prepareStatement(sql);
+        ResultSet result = statement2.executeQuery(sql);
+        int order_id = 0;
+        while (result.next()) {
+            order_id = result.getInt(1);
+        }
+        return order_id;
+    }
+
     public static ArrayList<OrderItem> getOrderItemsFromId(int orderId, Connection connection) {
         ArrayList<OrderItem> orderItems = new ArrayList<>();
 
@@ -53,7 +94,6 @@ public class OrderDAO {
                 Time time = result.getTime(4);
                 int addressId = result.getInt(5);
 
-                
                 order = new Order(orderId, customerId, paymentMethod, status, time, addressId);
             }
 
@@ -68,10 +108,9 @@ public class OrderDAO {
         ArrayList<Order> orders = new ArrayList<>();
         try {
             String sql = "SELECT order_id, user_id, payment_method, status, time, address_id " +
-            "FROM `Order` o INNER JOIN `Contain` c ON o.order_id = c.order_id " +
-            "INNER JOIN `Product` p ON p.product_id = c.product_id " +
-            "WHERE p.shop_id = ? AND status = ?";
-
+                    "FROM `Order` o INNER JOIN `Contain` c ON o.order_id = c.order_id " +
+                    "INNER JOIN `Product` p ON p.product_id = c.product_id " +
+                    "WHERE p.shop_id = ? AND status = ?";
 
             PreparedStatement statement = connection.prepareStatement(sql);
             statement.setInt(1, shop.getShopId());
@@ -86,7 +125,6 @@ public class OrderDAO {
                 Time time = result.getTime(5);
                 int addressId = result.getInt(6);
 
-           
                 orders.add(new Order(orderId, customerId, paymentMethod, status, time, addressId));
             }
 
@@ -112,18 +150,18 @@ public class OrderDAO {
             return false;
         }
     }
-    
+
     public static boolean acceptOrder(int orderId, Connection connection) {
         return changeStatus(orderId, "Accepted", connection);
-        
+
     }
 
     public static boolean rejectOrder(int orderId, Connection connection) {
         return changeStatus(orderId, "Rejected", connection);
     }
 
-    
-    // public static ArrayList<Order> createOrders(int customerId, int addressId, String paymentMethod, ArrayList<Product> products, Connection connection) {
-    //     Or
+    // public static ArrayList<Order> createOrders(int customerId, int addressId,
+    // String paymentMethod, ArrayList<Product> products, Connection connection) {
+    // Or
     // }
 }
