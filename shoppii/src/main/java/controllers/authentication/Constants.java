ba032a415aee8fb4b9b76c14f3b9dd0afc51b968
VVD <daovo739@gmail.com>
2022-10-11 00:52:42 +0700

    change URL google auth

--- shoppii/src/main/java/controllers/authentication/Constants.java
+++ shoppii/src/main/java/controllers/authentication/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 
     public static String GOOGLE_CLIENT_SECRET = "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";
 
-    public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/shoppii/api/login-google";
+    public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/shoppii/api/googleAuthentication";
 
     public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
 
