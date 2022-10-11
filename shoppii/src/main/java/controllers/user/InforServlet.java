574f0594e65e2224231dadb3f5a72995b5ee87f3
Potter <quanpotterha@gmail.com>
2022-10-11 11:19:58 +0700

    UpdateInfor

--- shoppii/src/main/java/controllers/user/InforServlet.java
+++ shoppii/src/main/java/controllers/user/InforServlet.java
@@ -2,9 +2,10 @@ package controllers.user;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
+
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -12,15 +13,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 import com.google.gson.Gson;
 
 import dao.AddressDAO;
 import dao.CustomerDAO;
 import dbconnect.DBConnect;
+import dbconnect.S3Util;
 import errors.ErrorHandle;
 import model.Address;
 import model.Customer;
+import utils.Utils;
 
 @MultipartConfig(fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
         maxFileSize = 1024 * 1024 * 1, // 1 MB
@@ -34,11 +38,17 @@ public class InforServlet extends HttpServlet {
         Connection connection = db.getConnection();
         resp.setContentType("application/json");
         try {
+            int userId = Integer.parseInt(req.getParameter("userId"));
             String name = req.getParameter("name");
+            String email = req.getParameter("email");
+            String phone = req.getParameter("phone");
             boolean sex = Boolean.parseBoolean(req.getParameter("sex"));
-            Date dob = new SimpleDateFormat("dd/MM/yyyy").parse(req.getParameter("dob"));
-            System.out.println(dob);
-            String json = gson.toJson(null);
+            Date dob = Date.valueOf(req.getParameter("dob"));
+            Part filePart = req.getPart("file");
+            String fileName = Utils.getFileName(filePart);
+            Customer updateCustomer = new Customer(userId,name, email, phone,dob, sex);
+            Customer customer =   CustomerDAO.updateInfo(updateCustomer, connection, fileName,filePart.getInputStream() );
+            String json = gson.toJson(customer);
             resp.setStatus(201);
             resp.getOutputStream().write(json.getBytes("UTF-8"));
         } catch (Exception e) {
--- shoppii/src/main/java/utils/Utils.java
+++ shoppii/src/main/java/utils/Utils.java
@@ -2,6 +2,8 @@ package utils;
 
 import java.util.Random;
 
+import javax.servlet.http.Part;
+
 public class Utils {
     private static String generateRandomString(int length) {
         int leftLimit = 48; // numeral '0'
@@ -23,5 +25,13 @@ public class Utils {
 
     public static String generateCode() {
         return generateRandomString(12);
+    }
+
+    public static String getFileName(Part part) {
+        String contentDisposition = part.getHeader("content-disposition");
+        int beginIndex = contentDisposition.indexOf("filename=") + 10;
+        int endIndex = contentDisposition.length() - 1;
+         
+        return contentDisposition.substring(beginIndex, endIndex);
     }
 }
