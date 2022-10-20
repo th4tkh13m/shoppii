ec90867e86ba0fb73521e7f06a1ddfabfc487a72
Khiem Ton <th4tkh13m@protonmail.com>
2022-10-20 21:13:17 +0700

    refactor

--- shoppii/src/main/java/controllers/shop/ShopRegisterServlet.java
+++ shoppii/src/main/java/controllers/shop/ShopRegisterServlet.java
@@ -11,8 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 import dao.RequestDAO;
 import dbconnect.DBConnect;
 import errors.ErrorHandle;
