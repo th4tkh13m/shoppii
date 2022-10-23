872be6e2415095ee7c4e63a365e556d4504759a1
Khiem Ton <th4tkh13m@protonmail.com>
2022-10-23 09:30:36 +0700

    update test

--- shoppii/src/test/java/dao/CartDAOTest.java
+++ shoppii/src/test/java/dao/CartDAOTest.java
@@ -76,8 +76,8 @@ public class CartDAOTest extends DBInfo {
             RequestDAO.acceptRequest(1, connection);
 
             // create product
-            Product product1 = new Product(1, 1, "iphone14", 999, 1, 2, "expensive");
-            Product product2 = new Product(2, 1, "iphone15", 999, 1, 2, "expensive");
+            Product product1 = new Product(1,"iphone14", 1, 999, "lol", CustomerDAO.getShopFromId(2, connection), CategoryDAO.getCategoryFromId(2, connection));
+            Product product2 = new Product(2, "iphone15", 1, 999, "nice", CustomerDAO.getShopFromId(2, connection), CategoryDAO.getCategoryFromId(2, connection));
             ProductDAO.addProduct(product1, connection);
             ProductDAO.addProduct(product2, connection);
             
@@ -106,9 +106,8 @@ public class CartDAOTest extends DBInfo {
             RequestDAO.acceptRequest(1, connection);
 
             // create product
-            Product product1 = new Product(1, 1, "iphone14", 999, 1, 2, "expensive");
-            Product product2 = new Product(2, 1, "iphone15", 999, 10, 2, "expensive");
-            ProductDAO.addProduct(product1, connection);
+            Product product1 = new Product(1,"iphone14", 1, 999, "lol", CustomerDAO.getShopFromId(2, connection), CategoryDAO.getCategoryFromId(2, connection));
+            Product product2 = new Product(2, "iphone15", 1, 999, "nice", CustomerDAO.getShopFromId(2, connection), CategoryDAO.getCategoryFromId(2, connection));
             ProductDAO.addProduct(product2, connection);
 
             CartDAO.addProductToCart(1, 1, 1, connection);
@@ -134,8 +133,8 @@ public class CartDAOTest extends DBInfo {
             RequestDAO.acceptRequest(1, connection);
 
             // create product
-            Product product1 = new Product(1, 1, "iphone14", 999, 1, 2, "expensive");
-            Product product2 = new Product(2, 1, "iphone15", 999, 10, 2, "expensive");
+            Product product1 = new Product(1,"iphone14", 1, 999, "lol", CustomerDAO.getShopFromId(2, connection), CategoryDAO.getCategoryFromId(2, connection));
+            Product product2 = new Product(2, "iphone15", 1, 999, "nice", CustomerDAO.getShopFromId(2, connection), CategoryDAO.getCategoryFromId(2, connection));
             ProductDAO.addProduct(product1, connection);
             ProductDAO.addProduct(product2, connection);
 
@@ -164,8 +163,8 @@ public class CartDAOTest extends DBInfo {
             RequestDAO.acceptRequest(1, connection);
 
             // create product
-            Product product1 = new Product(1, 1, "iphone14", 999, 1, 2, "expensive");
-            Product product2 = new Product(2, 1, "iphone15", 999, 10, 2, "expensive");
+            Product product1 = new Product(1,"iphone14", 1, 999, "lol", CustomerDAO.getShopFromId(2, connection), CategoryDAO.getCategoryFromId(2, connection));
+            Product product2 = new Product(2, "iphone15", 1, 999, "nice", CustomerDAO.getShopFromId(2, connection), CategoryDAO.getCategoryFromId(2, connection));
             ProductDAO.addProduct(product1, connection);
             ProductDAO.addProduct(product2, connection);
 
