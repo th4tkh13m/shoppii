73d71b8c2d8040c844762b8a1dc89f32c9f0cf3b
Khiem Ton <th4tkh13m@protonmail.com>
2022-10-30 08:33:19 +0700

    feat: shop statistics

--- shoppii/src/main/java/controllers/shop/ShopStatistics.java
+++ shoppii/src/main/java/controllers/shop/ShopStatistics.java
@@ -1,7 +1,39 @@
 package controllers.shop;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.sql.Date;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
+        maxFileSize = 1024 * 1024 * 1, // 1 MB
+        maxRequestSize = 1024 * 1024 * 1 // 1 MB
+)
 public class ShopStatistics extends HttpServlet {
-    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        Gson gson = new Gson();
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("application/json");
+        try {
+            
+
+
+            // Change this later
+            Date startDate = Date.valueOf("");
+            Date endDate = Date.valueOf("");
+
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
 }
