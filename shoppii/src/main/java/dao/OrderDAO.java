d844126b4f17a86897f32b3546cab0b912a1d6a1
Hoai Nhan <hoainhan.lhp@gmail.com>
2022-11-07 22:11:30 +0700

    fix getOrdersOfUser

--- shoppii/src/main/java/dao/OrderDAO.java
+++ shoppii/src/main/java/dao/OrderDAO.java
@@ -16,9 +16,10 @@ import model.Product;
 import model.Shop;
 
 public class OrderDAO {
-    public static ArrayList<Order> addOrder(HashMap<Shop, HashMap<Product, Integer>> confirmedProducts, int userId, String paymentMethod, int addressId, Connection connection) throws SQLException {
+    public static ArrayList<Order> addOrder(HashMap<Shop, HashMap<Product, Integer>> confirmedProducts, int userId,
+            String paymentMethod, int addressId, Connection connection) throws SQLException {
         ArrayList<Order> orders = new ArrayList<>();
-        // Tao order theo shop, moi shop 1 ma order 
+        // Tao order theo shop, moi shop 1 ma order
         for (Shop shop : confirmedProducts.keySet()) {
             String sql1 = "INSERT INTO `Order` (user_id, payment_method, address_id) VALUES(?,?,?)";
             PreparedStatement statement1 = connection.prepareStatement(sql1);
@@ -55,70 +56,71 @@ public class OrderDAO {
 
     public static ArrayList<OrderItem> getOrderItemsFromId(int orderId, Connection connection) throws SQLException {
         ArrayList<OrderItem> orderItems = new ArrayList<>();
-            String sql = "SELECT product_id, quantity, price FROM `Contain` WHERE order_id = ?";
-            PreparedStatement statement = connection.prepareStatement(sql);
+        String sql = "SELECT product_id, quantity, price FROM `Contain` WHERE order_id = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
 
-            statement.setInt(1, orderId);
+        statement.setInt(1, orderId);
 
-            ResultSet result = statement.executeQuery();
-            while (result.next()) {
-                int productId = result.getInt(1);
-                int quantity = result.getInt(2);
-                int price = result.getInt(3);
-                Product product = ProductDAO.getProductFromId(productId, connection);
-                orderItems.add(new OrderItem(product, quantity, price));
-            }
-            return orderItems;
+        ResultSet result = statement.executeQuery();
+        while (result.next()) {
+            int productId = result.getInt(1);
+            int quantity = result.getInt(2);
+            int price = result.getInt(3);
+            Product product = ProductDAO.getProductFromId(productId, connection);
+            orderItems.add(new OrderItem(product, quantity, price));
+        }
+        return orderItems;
     }
 
     public static Order getOrderFromId(int orderId, Connection connection) throws SQLException {
         Order order = null;
-            String sql = "SELECT user_id, payment_method, status, time, address_id FROM `Order` WHERE order_id = ?";
-            PreparedStatement statement = connection.prepareStatement(sql);
+        String sql = "SELECT user_id, payment_method, status, time, address_id FROM `Order` WHERE order_id = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
 
-            statement.setInt(1, orderId);
+        statement.setInt(1, orderId);
 
-            ResultSet result = statement.executeQuery();
-            while (result.next()) {
-                int customerId = result.getInt(1);
-                String paymentMethod = result.getString(2);
-                String status = result.getString(3);
-                Timestamp time = result.getTimestamp(4);
-                Address address = AddressDAO.getAddressFromId(result.getInt(5), connection);
+        ResultSet result = statement.executeQuery();
+        while (result.next()) {
+            int customerId = result.getInt(1);
+            String paymentMethod = result.getString(2);
+            String status = result.getString(3);
+            Timestamp time = result.getTimestamp(4);
+            Address address = AddressDAO.getAddressFromId(result.getInt(5), connection);
 
-                order = new Order(orderId, customerId, paymentMethod, status, time, address);
-            }
+            order = new Order(orderId, customerId, paymentMethod, status, time, address);
+        }
 
-            return order;
+        return order;
     }
 
-    public static ArrayList<Order> getOrdersByShop(int shopId, String statusFilter, Connection connection) throws SQLException {
+    public static ArrayList<Order> getOrdersByShop(int shopId, String statusFilter, Connection connection)
+            throws SQLException {
         ArrayList<Order> orders = new ArrayList<>();
-            String sql = "SELECT DISTINCT o.order_id, user_id, payment_method, status, time, address_id " +
-                    "FROM `Order` o INNER JOIN `Contain` c ON o.order_id = c.order_id " +
-                    "INNER JOIN `Product` p ON p.product_id = c.product_id " +
-                    "WHERE p.shop_id = ? AND status = ?";
+        String sql = "SELECT DISTINCT o.order_id, user_id, payment_method, status, time, address_id " +
+                "FROM `Order` o INNER JOIN `Contain` c ON o.order_id = c.order_id " +
+                "INNER JOIN `Product` p ON p.product_id = c.product_id " +
+                "WHERE p.shop_id = ? AND status = ?";
 
-            PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setInt(1, shopId);
-            if (statusFilter == null) {
-                statement.setString(2, "%");
-            } else
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, shopId);
+        if (statusFilter == null) {
+            statement.setString(2, "%");
+        } else
             statement.setString(2, statusFilter);
-            ResultSet result = statement.executeQuery();
+        ResultSet result = statement.executeQuery();
 
-            while (result.next()) {
-                int orderId = result.getInt(1);
-                int customerId = result.getInt(2);
-                String paymentMethod = result.getString(3);
-                String status = result.getString(4);
-                Timestamp time = result.getTimestamp(5);
-                Address address = AddressDAO.getAddressFromId(result.getInt(6), connection);
+        while (result.next()) {
+            int orderId = result.getInt(1);
+            int customerId = result.getInt(2);
+            String paymentMethod = result.getString(3);
+            String status = result.getString(4);
+            Timestamp time = result.getTimestamp(5);
+            Address address = AddressDAO.getAddressFromId(result.getInt(6), connection);
 
-                orders.add(new Order(orderId, customerId, paymentMethod, status, time, address));
-            }
+            orders.add(new Order(orderId, customerId, paymentMethod, status, time, address));
+        }
 
-            return orders;
+        return orders;
     }
 
     private static Order changeStatus(int orderId, String status, Connection connection) throws SQLException {
@@ -130,22 +132,27 @@ public class OrderDAO {
 
         statement.executeUpdate();
         return getOrderFromId(orderId, connection);
-            
+
     }
 
-    public static ArrayList<Order> getOrdersOfUser(int userId, String status, Connection connection) throws SQLException {
-        String sql = "SELECT order_id FROM `Order` WHERE user_id = ? AND LOWER(`status`) LIKE ?";
-
-        PreparedStatement statement = connection.prepareStatement(sql);
-        statement.setInt(1, userId);
-        if (status == null) {
-            statement.setString(2, "%");
-        } else {
-            statement.setString(2, status);
-        }
-        System.out.println(statement);
-        ResultSet result = statement.executeQuery();
+    public static ArrayList<Order> getOrdersOfUser(int userId, String status, Connection connection)
+            throws SQLException {
         ArrayList<Order> orders = new ArrayList<>();
+        ResultSet result;
+        PreparedStatement statement;
+        if (status == null) {
+            String sql = "SELECT order_id FROM `Order` WHERE user_id = ?";
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, userId);
+            result = statement.executeQuery();
+        } else {
+            String sql = "SELECT order_id FROM `Order` WHERE user_id = ? AND LOWER(`status`) LIKE ?";
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, userId);
+            statement.setString(2, status);
+            result = statement.executeQuery();
+        }
+
         while (result.next()) {
             orders.add(getOrderFromId(result.getInt(1), connection));
         }
@@ -161,7 +168,8 @@ public class OrderDAO {
         return changeStatus(orderId, "Rejected", connection);
     }
 
-    public static HashMap<Order, HashMap<Product, Integer>> getOrderWithItemsofUser(int userId, String status, Connection connection) throws SQLException {
+    public static HashMap<Order, HashMap<Product, Integer>> getOrderWithItemsofUser(int userId, String status,
+            Connection connection) throws SQLException {
         ArrayList<Order> orders = getOrdersOfUser(userId, status, connection);
         HashMap<Order, HashMap<Product, Integer>> orderMap = new HashMap<>();
 
@@ -175,12 +183,11 @@ public class OrderDAO {
 
             while (result.next()) {
                 Product product = ProductDAO.getProductFromId(result.getInt(1), connection);
-                ArrayList<String> images =
-                    S3Util.listPhotos("products/" + product.getProductId() + "/");
-                    System.out.println(images);
+                ArrayList<String> images = S3Util.listPhotos("products/" + product.getProductId() + "/");
+                System.out.println(images);
                 ArrayList<String> imagesUrl = new ArrayList<>();
                 if (images.size() > 0) {
-                    
+
                     imagesUrl.add(images.get(0));
                 }
                 product.setImages(imagesUrl);
@@ -194,7 +201,8 @@ public class OrderDAO {
         return orderMap;
     }
 
-    public static HashMap<Order, HashMap<Product, Integer>> getOrderWithItemsofShop(int shopId, String status, Connection connection) throws SQLException {
+    public static HashMap<Order, HashMap<Product, Integer>> getOrderWithItemsofShop(int shopId, String status,
+            Connection connection) throws SQLException {
         ArrayList<Order> orders = getOrdersByShop(shopId, status, connection);
         HashMap<Order, HashMap<Product, Integer>> orderMap = new HashMap<>();
 
@@ -208,12 +216,11 @@ public class OrderDAO {
 
             while (result.next()) {
                 Product product = ProductDAO.getProductFromId(result.getInt(1), connection);
-                ArrayList<String> images =
-                    S3Util.listPhotos("products/" + product.getProductId() + "/");
-                    System.out.println(images);
+                ArrayList<String> images = S3Util.listPhotos("products/" + product.getProductId() + "/");
+                System.out.println(images);
                 ArrayList<String> imagesUrl = new ArrayList<>();
                 if (images.size() > 0) {
-                    
+
                     imagesUrl.add(images.get(0));
                 }
                 product.setImages(imagesUrl);
